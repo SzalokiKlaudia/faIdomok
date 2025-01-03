3bc0a591d17babf072516e67b8d3fc2cf6cab0c6
SzalokiKlaudia <szaloki.klaudia90@gmail.com>
2025-01-03 22:50:59 +0100

    add more methods

--- src/main/FaIdomok.java
+++ src/main/FaIdomok.java
@@ -4,15 +4,98 @@
  */
 package main;
 
+import classes.Gomb;
+import classes.Hasab;
+import classes.Idom;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  *
  * @author Klau
  */
 public class FaIdomok {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+    private List<Idom>idomok;
+
+    public FaIdomok() {
+        this.idomok = new ArrayList<>();
+    }
+    
+    public double idomoksulya(){
+        
+        double osszSuly = 0;
+        
+        for (Idom idom : idomok) {
+            osszSuly += idom.suly(idom.terfogat());
+            
+            
+        }
+        return osszSuly;
+    }
+    
+    public double gombokSulya(){
+        double osszSuly = 0;
+        
+        for (Idom idom : idomok) {
+            if(idom instanceof Gomb){
+                osszSuly += idom.suly(idom.terfogat());
+                
+            }
+            
+        }
+        return osszSuly;
+        
+        
+    }
+    
+    public void minMaxTerfogat(){
+        Idom legkisebb = null;
+        Idom legnagyobb = null;
+        
+        for (Idom idom : idomok) {
+            if(legkisebb == null || idom.terfogat()< legkisebb.terfogat()){
+                legkisebb = idom;
+            }  if(legnagyobb == null || idom.terfogat()> legnagyobb.terfogat()){
+                legnagyobb = idom;
+            }
+            
+        }
+        
+        System.out.println("A legkisebb térfogatú idom: " + legkisebb);
+        System.out.println("A legnagyobb térfogatú idom: " + legnagyobb);
+
+        
+        
+        
+    }
+    
+    public void kulonbozoSugaruGombok(){
+        
+        Set<Double> sugarak = new HashSet<>();
+        
+        
+        
+    }
+    
+    public void run(){
+        
+        idomok.add(new Gomb(2.3));
+        idomok.add(new Gomb(2.5));
+        idomok.add(new Gomb(3.3));
+        idomok.add(new Hasab(2.1, 3.1, 4.4));
+        idomok.add(new Hasab(4.4, 1.5, 4.0));
+        idomok.add(new Hasab(2.2, 3.2, 4.2));
+        
+       
+    }
+    
+    
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
